24111eab052905062ddcfefc2abe2137693598df
Mirko Sale <pb51vab@etmlnet.local>
2022-04-04 15:52:34 +0200

    Changed pages, added controllers, CSS, etc.

---
+++ controller/UserController.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * ETML
+ * Auteur : Cindy Hardegger
+ * Date: 22.01.2019
+ * Controler pour gérer les recettes
+ */
+
+include_once 'model/UserRepository.php';
+
+class UserController extends Controller {
+
+    /**
+     * Permet de choisir l'action à effectuer
+     *
+     * @return mixed
+     */
+    public function display() {
+
+        $action = $_GET['action'] . "Action";
+
+        // Appelle une méthode dans cette classe (ici, ce sera le nom + action (ex: listAction, detailAction, ...))
+        return call_user_func(array($this, $action));
+    }
+
+    private function connexionAction()
+    {
+        if (isset($_SESSION['useLogin']))
+        {
+            $view = file_get_contents('view/page/user/loginForm.php');
+        }
+        else
+        {
+            $view = file_get_contents('view/page/user/logoutForm.php');
+        }
+        ob_start();
+        eval('?>' . $view);
+        $content = ob_get_clean();
+
+        return $content;
+    }
+
+        /**
+     * Check Form submit
+     *
+     * @return string
+     */
+    private function loginAction()
+    {
+        $database = new Database();
+        $response = $database->getOneUser($_POST['user'], $_POST['password']);
+        
+        // Si la connexion n'est pas faux
+        if ($response != false) {
+            //Ajout la session dans la bd
+            $idSession = $database->addSession($response[0]['idUser']);
+        
+            // Création cookie de connexion
+            setcookie('idSession', $idSession, time() + 30 * 24 * 60 * 60);
+        }
+        // Redirection
+        header('Location: index.php');
+    }
+
+    private function logoutAction()
+    {
+
+    }
+}
---
+++ view/page/user/loginForm.php
@@ -0,0 +1,28 @@
+<body class="sub_page">
+    <section class="book_section layout_padding">
+        <div class="container">
+            <div class="heading_container">
+                <h2>
+                    Connexion
+                </h2>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form_container">
+                        <form action="index.php?controller=home&action=login" method="post">
+                            <label for="user"></label>
+                            <input type="text" name="user" class="form-control" placeholder="Login">
+                            <label for="password"></label>
+                            <input type="password" name="password" class="form-control" placeholder="Mot de passe">
+                            <div class="btn_box">
+                                <button type="submit">Se connecter</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+    </section>
+    </div>
+</body>
+
+</html>
