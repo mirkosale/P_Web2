1460051b6565908223df0c7cc69f6077875745ca
Westim <pg44yzv@eduvaud.ch>
2022-05-16 17:21:58 +0200

    10 eco conseption rules

--- view/page/home/contact.php
+++ view/page/home/contact.php
@@ -1,48 +1,49 @@
 <body class="sub_page">
-  <section class="book_section layout_padding">
-    <div class="container">
-      <div class="heading_container">
-        <h2>
-          Contact 
-        </h2>
-      </div>
-      <div class="row">
-        <div class="col-md-6">
-          <div class="form_container">
-            <form action="?controller=recipe&action=checkContact">
-              <div class="was-validated">
-                <label for="name"><a style="color:red">*</a>Nom</label>
-                <input type="text" class="form-control" id="firstname" name="firstname" />
-              </div>
-              <div>
-                <label for="typedish"><a style="color:red">*</a>Email</label>
-                <input type="email" class="form-control" id="typedish" name="typedish" />
-              </div>
-              <div>
-                <label for="itemList"><a style="color:red">*</a>Adresse</label>
-                <textarea id="itemList" name="itemList"></textarea>
-              </div>
-              <div>
-                <label for="itemList"><a style="color:red">*</a>Numero de téléphone</label>
-                <textarea id="itemList" name="itemList"></textarea>
-              </div>
-              <div>
-                <label for="preparation"><a style="color:red">*</a>message :</label>
-						    <textarea id="preparation" name="preparation" rows="10"></textarea>
-					    </div>
-              <div class="btn_box">
-                <button>
-                  Book Now
-                </button>
-              </div>
-            </form>
-          </div>
+    <section class="book_section layout_padding">
+        <div class="container">
+            <div class="heading_container">
+                <h2>
+                    Contact
+                </h2>
+                <p style="color:red">*Informations obligatoires</p>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form_container">
+                        <form action="?controller=recipe&action=checkContact">
+                            <div>
+                                <label for="name"><a style="color:red">*</a>Nom</label>
+                                <input type="text" class="form-control" id="name" name="name" />
+                            </div>
+                            <div>
+                                <label for="email"><a style="color:red">*</a>Email</label>
+                                <input type="email" class="form-control" id="email" name="email" />
+                            </div>
+                            <div>
+                                <label for="adress">Adresse</label>
+                                <textarea id="adress" name="adress"></textarea>
+                            </div>
+                            <div>
+                                <label for="phoneNumber">Numero de téléphone</label>
+                                <input type="text" class="form-control" id="phoneNumber" name="phoneNumber" />
+                            </div>
+                            <div>
+                                <label for="message"><a style="color:red">*</a>Message</label>
+                                <textarea id="message" name="message" rows="10"></textarea>
+                            </div>
+                            <div class="btn_box">
+                                <button type="submit">
+                                    Envoyer
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="map_container ">
+                        <div id="googleMap"></div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-md-6">
-          <div class="map_container ">
-            <div id="googleMap"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+    </section>
--- view/page/recipe/detail.php
+++ view/page/recipe/detail.php
@@ -1,34 +1,34 @@
 <body class="sub_page">
-	<div class="container">
+    <div class="container food_section">
 
-		<div class="text-topRight">
-			<div class="imgDetail">
-				<?php echo '<img class="imgDetail" src="./resources/images/' . $recipe[0]['recImage'] . '" alt="Image de : ' . $recipe[0]['recName'] . '">'; ?>
-				<?php
+        <div class="text-topRight">
+            <div class="imgDetail">
+                <?php echo '<img class="imgDetail" src="./resources/images/' . $recipe[0]['recImage'] . '" alt="Image de : ' . $recipe[0]['recName'] . '">'; ?>
+                <?php
 				?>
-			</div>
-		</div>
-		<?php
+            </div>
+        </div>
+        <?php
 		echo "<h1>" . $recipe[0]["recName"] . "</h1>";
 		?>
 
-		<?php
+        <?php
 		echo "<h2>Type : " . $recipe[0]["typName"] . "</h2>";
 		?>
 
-		<!-- Three columns of text below the carousel -->
-		<div class="row">
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-				<div class="parafsd">
-					<?php
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="parafsd">
+                    <?php
 						echo '<h3>Ingredients</h3> <p>' . $recipe[0]["recListOfItems"] . '</p>';
 						echo '<h3>Préparation</h3> <p>' . $recipe[0]["recPreparation"] . '</p>';
 					?>
-				</div>
-			</div>
-		</div>
-		<!-- Affichage de la note -->
-		<?php
+                </div>
+            </div>
+        </div>
+        <!-- Affichage de la note -->
+        <?php
 			if (isset($note[0]['notStars'])) {
 				echo '<p>Votre note actuelle : ' . $note[0]["notStars"] . '</p>';
 				echo '<a href="?controller=note&action=delete&id=' . $note[0]['idNote'] . '">Supprimer la note</a>';
@@ -36,26 +36,24 @@
 				echo "Attribuer une note";
 		?>
 
-			<!-- https://www.foolishdeveloper.com/2022/01/5-star-rating-html-css.html -->
-			<div class="row">
-				<div class="col-md-6">
-					<div class="form_container">
-						<?php echo '<form action="?controller=note&action=add&id=' . $recipe[0]['idRecipe'] . '">'; ?>
-							<div class="btn-box">
-								<button type="submit">Envoyer votre note</button>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		<?php } ?>
+        <!-- https://www.foolishdeveloper.com/2022/01/5-star-rating-html-css.html -->
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form_container">
+                    <?php echo '<form action="?controller=note&action=add&id=' . $recipe[0]['idRecipe'] . '">'; ?>
+                    <div class="btn-box">
+                        <button type="submit">Envoyer votre note</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
 
-		<div class="row">
-			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-				<div class="btn-box">
-					<a href="index.php?controller=recipe&action=list">Retour à la liste des Recettes</a>
-				</div>
-			</div>
-		</div>
-	</div>
+        <div class="row">
+            <div class="btn-box">
+                <a href="index.php?controller=recipe&action=list">Retour à la liste des Recettes</a>
+            </div>
+        </div>
+    </div>
 </body>
